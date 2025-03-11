80b87e88b6998142b458d301ac60aa268254e3a6
meszyyy <meszarostamas1989@gmail.com>
2025-03-11 15:50:55 +0100

    Updates

---
+++ precache-manifest.f85a8df59dfb00d513157d1885b86ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5358cb76f91d418dcc5b",
+    "url": "/alizbeauty/static/css/main.76f6cf2a.chunk.css"
+  },
+  {
+    "revision": "5358cb76f91d418dcc5b",
+    "url": "/alizbeauty/static/js/main.93b8e693.chunk.js"
+  },
+  {
+    "revision": "6b6f3c94a717df09eb91",
+    "url": "/alizbeauty/static/js/runtime~main.bd3a6182.js"
+  },
+  {
+    "revision": "d79ba5c7030e3ea7a488",
+    "url": "/alizbeauty/static/js/2.aca2b68d.chunk.js"
+  },
+  {
+    "revision": "2971b56a41c01d0281a6ada601caa9be",
+    "url": "/alizbeauty/static/media/LeJourScript.2971b56a.otf"
+  },
+  {
+    "revision": "5fa8bdd9a732c13c2c8881efb1f07be1",
+    "url": "/alizbeauty/index.html"
+  }
+];
